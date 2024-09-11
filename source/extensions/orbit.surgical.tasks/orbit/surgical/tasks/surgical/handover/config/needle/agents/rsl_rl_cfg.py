14703717e490c2b1018eae5f37d7475e8156c539
alina_marcali <alina_marchenko@outlook.com>
2024-09-11 15:32:04 +0100

    agent

--- source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/handover/config/needle/agents/rsl_rl_cfg.py
+++ source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/handover/config/needle/agents/rsl_rl_cfg.py
@@ -12,9 +12,8 @@ from omni.isaac.lab_tasks.utils.wrappers.rsl_rl import (
 )
 
 
-@configclass
 class HandoverNeedlePPORunnerCfg(RslRlOnPolicyRunnerCfg):
-    num_steps_per_env = 48
+    num_steps_per_env = 24
     max_iterations = 1500
     save_interval = 100
     experiment_name = "dual_needle_handover"
@@ -29,13 +28,13 @@ class HandoverNeedlePPORunnerCfg(RslRlOnPolicyRunnerCfg):
         value_loss_coef=1.0,
         use_clipped_value_loss=True,
         clip_param=0.2,
-        entropy_coef=1e-3,
+        entropy_coef=0.006,
         num_learning_epochs=5,
         num_mini_batches=4,
         learning_rate=1.0e-4,
         schedule="adaptive",
-        gamma=0.99,
+        gamma=0.98,
         lam=0.95,
-        desired_kl=0.02,
+        desired_kl=0.01,
         max_grad_norm=1.0,
     )
