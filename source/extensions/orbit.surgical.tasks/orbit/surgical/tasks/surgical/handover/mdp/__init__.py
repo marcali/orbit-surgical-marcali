75cd113dd0d237245cc420e49d54d13f1c275801
alina_marcali <alina_marchenko@outlook.com>
2024-09-11 15:11:24 +0100

    added mdp config

--- source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/handover/mdp/__init__.py
+++ source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/handover/mdp/__init__.py
@@ -5,4 +5,9 @@
 
 """This sub-module contains the functions that are specific to the handover environments."""
 
+
 from omni.isaac.lab.envs.mdp import *  # noqa: F401, F403
+
+from .observations import *  # noqa: F401, F403
+from .rewards import *  # noqa: F401, F403
+from .terminations import *  # noqa: F401, F403
